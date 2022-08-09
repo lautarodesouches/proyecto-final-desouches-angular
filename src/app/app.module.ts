6bee60292aab5cb8a492d09203cdd92a5cc52433
Lautaro Desouches <lautarodesouches@outlook.com>
2022-08-09 13:09:23 -0300

    Bootstrap - Material - Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { ListaAlumnosComponent } from './components/lista-alumnos/lista-alumnos.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    ToolbarComponent,
+    ListaAlumnosComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
