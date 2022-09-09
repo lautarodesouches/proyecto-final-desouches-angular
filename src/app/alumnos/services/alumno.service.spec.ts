9d6097934f8c1554c95a05c312391205c9563bf7
Lautaro Desouches <lautarodesouches@outlook.com>
2022-09-08 23:30:22 -0300

    Tests

--- src/app/alumnos/services/alumno.service.spec.ts
+++ src/app/alumnos/services/alumno.service.spec.ts
@@ -1,8 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { AlumnoService } from './alumno.service';
+import { of } from 'rxjs';
 
 describe('AlumnoService', () => {
+
+  let httpClientSpy: { get: jasmine.Spy }
   let service: AlumnoService;
 
   beforeEach(async () => {
@@ -12,11 +15,63 @@ describe('AlumnoService', () => {
       providers: [AlumnoService]
     }).compileComponents();
 
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(AlumnoService);
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ]
+    });
+
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get'])
+    service = new AlumnoService(httpClientSpy as any)
+    
   });
 
-  it('should be created', () => {
+  it('Crear servicio', () => {
     expect(service).toBeTruthy();
   });
+
+  it('Retornar lista alumnos', (done: DoneFn) => {
+
+    const mockDatos = [
+      {
+        "nombre": "Melyna",
+        "apellido": "Bogan",
+        "email": "Melyna_Bogan@gmail.com",
+        "telefono": "1-550-424-7581 x76478",
+        "dni": 56463,
+        "pais": "RE",
+        "activo": false,
+        "id": "22"
+      },
+      {
+        "nombre": "Jennie",
+        "apellido": "VonRueden",
+        "email": "Jennie.VonRueden45@gmail.com",
+        "telefono": "804.549.7432",
+        "dni": 56312,
+        "pais": "AR",
+        "activo": true,
+        "id": "23"
+      },
+      {
+        "nombre": "Earl",
+        "apellido": "Morissette",
+        "email": "Davin92@hotmail.com",
+        "telefono": "1-968-955-8337 x0867",
+        "dni": 93748,
+        "pais": "SG",
+        "activo": true,
+        "id": "24"
+      }
+    ]
+
+    httpClientSpy.get.and.returnValue(of(mockDatos))
+
+    service.obtenerAlumnos().subscribe(alumnos => {
+      expect(alumnos).toEqual(mockDatos)
+      done()
+    })
+
+  });
+
 });
---
+++ src/app/auth/components/login/login.component.spec.ts
@@ -0,0 +1,131 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RouterTestingModule } from '@angular/router/testing';
+import { MatInputModule } from '@angular/material/input';
+import { MatDialog, MatDialogModule, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login.component';
+import { AuthService } from 'src/app/core/services/auth.service';
+import { of } from 'rxjs';
+import { Router } from '@angular/router';
+
+describe('LoginComponent', () => {
+
+  let httpClientSpy: { get: jasmine.Spy }
+  let service: AuthService;
+  let component: LoginComponent;
+  let fixture: ComponentFixture<LoginComponent>;
+  let router: Router
+
+  beforeEach(async () => {
+
+    await TestBed.configureTestingModule({
+
+      imports: [
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+        MatInputModule,
+        MatDialogModule,
+        MatButtonModule,
+        FormsModule,
+        RouterTestingModule,
+        HttpClientTestingModule
+      ],
+      declarations: [
+        LoginComponent
+      ],
+      providers: [
+        {
+          provide: MatDialog, value: {}
+        },
+        {
+          provide: MAT_DIALOG_DATA, useValue: {}
+        },
+        AuthService
+      ]
+
+    }).compileComponents();
+
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get'])
+    service = new AuthService(httpClientSpy as any, router)
+
+    fixture = TestBed.createComponent(LoginComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+
+  });
+
+  it('Crea el componente', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('El formulario es invalido si ambos campos estan vacios', () => {
+    const formulario = component.formulario
+    const usuario = formulario.controls['usuario']
+    const contrasenia = formulario.controls['contrasenia']
+
+    usuario.setValue('')
+    contrasenia.setValue('')
+
+    expect(formulario.invalid).toBeTruthy();
+  });
+
+  it('El formulario es invalido si el usuario se encuentra vacio', () => {
+    const formulario = component.formulario
+    const usuario = formulario.controls['usuario']
+
+    usuario.setValue('')
+
+    expect(formulario.invalid).toBeTruthy();
+  });
+
+  it('El formulario es invalido si la contraseña se encuentra vacia', () => {
+    const formulario = component.formulario
+    const contrasenia = formulario.controls['contrasenia']
+
+    contrasenia.setValue('')
+
+    expect(formulario.invalid).toBeTruthy();
+  });
+
+  /* it('Loggear con credenciales validas', (done: DoneFn) => {
+
+    const formulario = component.formulario
+    const usuario = formulario.controls['usuario']
+    const contrasenia = formulario.controls['contrasenia']
+
+    usuario.setValue('test')
+    contrasenia.setValue('12345')
+
+    const mockDatos = [
+      {
+        "usuario": "test",
+        "admin": true,
+        "contrasenia": "12345",
+        "email": "test@gmail.com",
+        "id": "1"
+      },
+      {
+        "usuario": "tost",
+        "admin": false,
+        "contrasenia": "6789",
+        "email": "Lewis88@hotmail.com",
+        "id": "2"
+      }
+    ]
+
+    httpClientSpy.get.and.returnValue(of(mockDatos))
+
+    component.login()
+
+    service.obtenerSesion().subscribe(e => {
+      expect(e.sesionActiva).toBeTruthy();
+      done()
+    })
+
+  }); */
+
+});
